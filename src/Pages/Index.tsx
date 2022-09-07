9836d685bd593ae2c5d154eaec2f7fc32a1d7f39
nirgoldman <refaelgold@gmail.com>
2022-09-07 18:04:33 +0300

    More tries to make variables persist value after refresh (Using useState).

--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -9,7 +9,10 @@ import {ItemType} from "../Interfaces/ItemType";
 function Index(){
     const {t} = useTranslation();
     const [data,setData]=useState([]);
-    const [totalTypeCounterArrState,setTotalTypeCounterArrState]=useState([]);
+    const [shirtCounterState,setShirtCounterState]=useState([]);
+    const [pantsCounterState,setPantsCounterState]=useState([]);
+    const [shoesCounterState,setShoesCounterState]=useState([]);
+
 
     const getData=()=>{
         fetch('https://run.mocky.io/v3/2d06d2c1-5a77-4ecd-843a-53247bcb0b94'
@@ -28,55 +31,51 @@ function Index(){
                 localStorage.setItem('items', JSON.stringify(itemDataJson));
             });
     }
-
-    const ItemTypeCounter=()=> {
-        let shirtCounter = 0;
-        let pantsCounter = 0;
-        let shoesCounter = 0;
-        const TotalTypeCounterArr:any= []
-        data && data.length > 0 && data.map(
-            (ItemData: ItemType, i) => {
-                switch (ItemData.type) {
-                    case 'pants':
-                        pantsCounter++;
-                        TotalTypeCounterArr[0]=pantsCounter;
-                        return ;
-                    case 'shirt':
-                        shirtCounter++;
-                        TotalTypeCounterArr[1]=shirtCounter;
-                        return ;
-                    case 'shoes':
-                        shoesCounter++;
-                        TotalTypeCounterArr[2]=shoesCounter;
-                        return;
-                    default:
-                        return;
-                }
-
-                // Will not work after one refresh only when I change the code So I use Local Storage
-                if (!(localStorage.getItem('pantsCounter'))){
-                    localStorage.setItem('pantsCounter',String(TotalTypeCounterArr[0]));
-                }
-                if (!(localStorage.getItem('shirtCounter'))){
-                    localStorage.setItem('shirtCounter',String(TotalTypeCounterArr[1]));
-                }
-                if (!(localStorage.getItem('shoesCounter'))){
-                    localStorage.setItem('shoesCounter',String(TotalTypeCounterArr[2]));
-                }
-
-                return;
-            });
-
-        setTotalTypeCounterArrState(TotalTypeCounterArr);
-        return;
-    }
-    // useEffect(callback, dependencies) is the hook that manages the side-effects in functional components.
-    // callback argument is a function to put the side-effect logic.
-    // dependencies is a list of dependencies of your side-effect: being props or state values.
     useEffect(()=>{
         getData()
-        ItemTypeCounter()
     },[])
+
+
+
+
+    let shirtCounter = 0;
+    let pantsCounter = 0;
+    let shoesCounter = 0;
+    const TotalTypeCounterArr:any= []
+    data && data.length > 0 && data.map(
+        (ItemData: ItemType, i) => {
+            switch (ItemData.type) {
+                case 'pants':
+                    pantsCounter++;
+                    TotalTypeCounterArr[0]=pantsCounter;
+                    return pantsCounter;
+                case 'shirt':
+                    shirtCounter++;
+                    TotalTypeCounterArr[1]=shirtCounter;
+                    return  shirtCounter;
+                case 'shoes':
+                    shoesCounter++;
+                    TotalTypeCounterArr[2]=shoesCounter;
+                    return shoesCounter;
+            }
+            return "Unsupported Data";
+        });
+    // Will not work after one refresh only when I change the code So I use Local Storage
+
+    if (!localStorage.getItem('pantsCounter')){
+        localStorage.setItem('pantsCounter',TotalTypeCounterArr[0]);
+    }
+    if (!localStorage.getItem('shirtCounter')){
+        localStorage.setItem('shirtCounter',TotalTypeCounterArr[1]);
+    }
+    if (!localStorage.getItem('shoesCounter')){
+        localStorage.setItem('shoesCounter',TotalTypeCounterArr[2]);
+    }
+
+    useEffect(()=>{
+       window.localStorage.setItem('pantsCounterState',TotalTypeCounterArr[0]);
+
+    },[shirtCounterState,pantsCounterState,shoesCounterState])
 
     return (
         <div>
