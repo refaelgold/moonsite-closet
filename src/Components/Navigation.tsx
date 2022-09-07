75d0d29031ac0cf2d8ce2db16ddacf77dec669dd
nirgoldman <refaelgold@gmail.com>
2022-09-07 15:56:03 +0300

    Remove git logo from Navigation

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -9,7 +9,6 @@ const NavbarComponent = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="/"><img src={require('../Images/git_logo.jpg')} width={200} height={50} alt=''/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="text-white">
