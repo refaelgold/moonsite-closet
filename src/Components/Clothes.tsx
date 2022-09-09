e50bdbcd07c4353c8297d5752c999e365d262229
nirgoldman <refaelgold@gmail.com>
2022-09-09 11:31:48 +0300

    Re-design cloth page for functionality with sidebar filters

---
+++ src/Components/Clothes.tsx
@@ -0,0 +1,54 @@
+import Item from "../Components/Item";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {useEffect,useState} from "react";
+import {useTranslation} from "react-i18next";
+import {ItemType} from "../Interfaces/ItemType";
+import { useNavigate} from "react-router-dom";
+
+
+
+function ClothesItems(){
+    const {t} = useTranslation();
+    let navigate = useNavigate();
+
+    const [data,setData]=useState([]);
+
+    const getData=()=>{
+        fetch('https://run.mocky.io/v3/2d06d2c1-5a77-4ecd-843a-53247bcb0b94'
+            ,{
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(itemDataJson) {
+                setData(itemDataJson)
+                localStorage.setItem('items', JSON.stringify(itemDataJson));
+            });
+    }
+    useEffect(()=>{
+        getData()
+    },[])
+
+    return (
+        <>
+                        {
+                            data && data.length>0 && data.map(
+                                (ItemData:ItemType,i)=>
+                                    <Col key={i}>
+                                        <Item key={i} id={ItemData.id}  type={ItemData.type} brand={ItemData.brand} size={ItemData.size} color={ItemData.color} />
+                                    </Col>
+                            )
+                        }
+        </>
+    );
+
+}
+
+export default ClothesItems;
---
+++ src/Components/FilterClothes.tsx
@@ -0,0 +1,20 @@
+import {useTranslation} from "react-i18next";
+import {useNavigate} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Clothes from "./Clothes";
+
+
+function ClothesItems(){
+    const {t} = useTranslation();
+
+    return (
+        <>
+            <h1>form</h1>
+        </>
+    );
+
+}
+
+export default ClothesItems;
